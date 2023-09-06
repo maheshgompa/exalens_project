64351b262c46f1c5e64914427815c4146a3e6c41
maheshgompa <gompamahesh369@gmail.com>
2023-09-06 16:21:22 +0530

    Update consumer.py

--- mqtt_subscriber/consumer.py
+++ mqtt_subscriber/consumer.py
@@ -4,12 +4,15 @@ import paho.mqtt.client as mqtt
 # MQTT broker settings
 broker_address = "localhost"  # Change this to your broker's IP address or hostname
 port = 1883
-topic = "test2/topic"  # Change this to the MQTT topic you want to subscribe to
-
+# topic = "test2/topic"  # Change this to the MQTT topic you want to subscribe to
+temperature_topic = "sensors/temperature"
+humidity_topic = "sensors/humidity"
 # MQTT on_connect callback
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker with code:", rc)
-    client.subscribe(topic)
+    # client.subscribe(topic)
+    client.subscribe(temperature_topic)
+    client.subscribe(humidity_topic)
 
 # MQTT on_message callback
 def on_message(client, userdata, message):
